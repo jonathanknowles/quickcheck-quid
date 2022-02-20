dec012a6e37a7bb9c9128629a9c547efa080cc8f
Jonathan Knowles <mail@jonathanknowles.net>
2022-02-20 06:26:56 +0000

    Add combinators and representations to public API.

--- src/quickcheck-quid/Test/QuickCheck/Quid.hs
+++ src/quickcheck-quid/Test/QuickCheck/Quid.hs
@@ -1,6 +1,22 @@
 module Test.QuickCheck.Quid
-    ( Quid )
+    (
+    -- * The Quid type
+      Quid
+
+    -- * Combinators
+    , Prefix
+    , Size
+
+    -- * Representations
+    , Latin
+    )
     where
 
 import Internal.Test.QuickCheck.Quid
     ( Quid )
+import Internal.Test.QuickCheck.Quid.Combinators.Prefix
+    ( Prefix )
+import Internal.Test.QuickCheck.Quid.Combinators.Size
+    ( Size )
+import Internal.Test.QuickCheck.Quid.Representations.Latin
+    ( Latin )
