44cd7dcb4e6d0cde08613ada49b3fecbdbd51c0f
Jonathan Knowles <mail@jonathanknowles.net>
2022-02-20 14:08:00 +0000

    Add example function-based property to module `Example`.

--- src/quickcheck-quid-example/Test/QuickCheck/Quid/Example.hs
+++ src/quickcheck-quid-example/Test/QuickCheck/Quid/Example.hs
@@ -1,14 +1,35 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingVia #-}
 
 module Test.QuickCheck.Quid.Example where
 
+import GHC.Generics
+    ( Generic )
+import Numeric.Natural
+    ( Natural )
 import Test.QuickCheck
-    ( Arbitrary )
+    ( Arbitrary
+    , CoArbitrary
+    , Fun
+    , Function
+    , Property
+    , applyFun
+    , label
+    , property
+    )
 import Test.QuickCheck.Quid
     ( Latin (..), Prefix (..), Quid (..), Size (..) )
 
-newtype ExampleQuid = ExampleQuid Quid
-    deriving (Eq, Ord)
-    deriving Arbitrary via (Size 256 Quid)
+newtype ExampleQuid = ExampleQuid {unExampleQuid :: Quid}
     deriving (Read, Show) via (Prefix "example-quid:" (Latin Quid))
+    deriving Arbitrary via (Size 256 Quid)
+    deriving CoArbitrary via Quid
+    deriving anyclass Function
+    deriving stock (Eq, Generic, Ord)
+
+prop_function :: Fun ExampleQuid Bool -> ExampleQuid -> Property
+prop_function f q =
+    label (show (applyFun f q)) $
+    property True
