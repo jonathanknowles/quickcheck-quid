ae615063e966e5801742fa1caaa8f0425ac32fd7
Jonathan Knowles <mail@jonathanknowles.net>
2022-03-27 03:16:21 +0000

    Provide `Num` instances for example newtypes.

--- src/quickcheck-quid-example/Test/QuickCheck/Quid/Example.hs
+++ src/quickcheck-quid-example/Test/QuickCheck/Quid/Example.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Test.QuickCheck.Quid.Example where
 
@@ -13,19 +14,21 @@ import Test.QuickCheck.Quid
     ( Decimal (..), Hexadecimal (..), Latin (..), Quid (..), Size (..) )
 
 newtype ExampleDecimalQuid = ExampleDecimalQuid (Decimal Quid)
+    deriving stock (Eq, Generic, Ord, Read, Show)
     deriving Arbitrary via (Size 256 Quid)
     deriving CoArbitrary via Quid
     deriving anyclass Function
-    deriving stock (Eq, Generic, Ord, Read, Show)
+    deriving newtype Num
 
 newtype ExampleHexadecimalQuid = ExampleHexadecimalQuid (Hexadecimal Quid)
+    deriving stock (Eq, Generic, Ord, Read, Show)
     deriving Arbitrary via (Size 256 Quid)
     deriving CoArbitrary via Quid
     deriving anyclass Function
-    deriving stock (Eq, Generic, Ord, Read, Show)
+    deriving newtype Num
 
 newtype ExampleLatinQuid = ExampleLatinQuid (Latin Quid)
+    deriving stock (Eq, Generic, Ord, Read, Show)
     deriving Arbitrary via (Size 256 Quid)
     deriving CoArbitrary via Quid
     deriving anyclass Function
-    deriving stock (Eq, Generic, Ord, Read, Show)
